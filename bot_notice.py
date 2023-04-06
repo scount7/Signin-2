debca7c9df109b8beb7abd49f36e627799e35992
FHYQ-Dong <FHYQ-Dong@outlook.com>
2023-04-06 09:40:50 +0800

    update

---
+++ bot_notice.py
@@ -0,0 +1,13 @@
+from telegram.ext import Updater
+from sys import argv
+
+def notice():
+    TOKEN = argv[1]
+    CHAT_ID = argv[2]
+    with open("result.txt", mode="r", encoding="utf-8") as f:
+        res = f.read()
+    myBot = Updater(TOKEN)
+    myBot.bot.send_message(
+        chat_id = CHAT_ID,
+        text = res
+    )
